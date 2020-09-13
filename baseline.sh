8f6388ddd65ae785709bcb07a0600763468b1736
htwang14 <htwang14@gmail.com>
2020-09-12 22:39:11 -0500

    add sript for baseline

---
+++ baseline.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+# CR=49.6%
+python dlrm_s_pytorch.py \
+    --arch-sparse-feature-size=16 \
+    --arch-mlp-bot="13-512-256-64-16" \
+    --arch-mlp-top="300-150-1" \
+    --data-generation=dataset \
+    --data-set=kaggle \
+    --raw-data-file=/hdd3/jiayi/kaggle/train.txt \
+    --processed-data-file=/hdd3/jiayi/kaggle/kaggleAdDisplayChallenge_processed.npz \
+    --loss-function=bce \
+    --round-targets=True \
+    --learning-rate=0.1 \
+    --mini-batch-size=128 \
+    --print-freq=1024 \
+    --print-time \
+    --test-mini-batch-size=16384 \
+    --test-num-workers=16 \
+    2>&1 | tee 300-150-1_lr0.1.log
+
+# CR=40.7%
+python dlrm_s_pytorch.py \
+    --arch-sparse-feature-size=16 \
+    --arch-mlp-bot="13-512-256-64-16" \
+    --arch-mlp-top="256-128-1" \
+    --data-generation=dataset \
+    --data-set=kaggle \
+    --raw-data-file=/hdd3/jiayi/kaggle/train.txt \
+    --processed-data-file=/hdd3/jiayi/kaggle/kaggleAdDisplayChallenge_processed.npz \
+    --loss-function=bce \
+    --round-targets=True \
+    --learning-rate=0.1 \
+    --mini-batch-size=128 \
+    --print-freq=1024 \
+    --print-time \
+    --test-mini-batch-size=16384 \
+    --test-num-workers=16 \
+    2>&1 | tee 256-128-1_lr0.1.log
+
+# CR=34.6%
+python dlrm_s_pytorch.py \
+    --arch-sparse-feature-size=16 \
+    --arch-mlp-bot="13-512-256-64-16" \
+    --arch-mlp-top="224-112-1" \
+    --data-generation=dataset \
+    --data-set=kaggle \
+    --raw-data-file=/hdd3/jiayi/kaggle/train.txt \
+    --processed-data-file=/hdd3/jiayi/kaggle/kaggleAdDisplayChallenge_processed.npz \
+    --loss-function=bce \
+    --round-targets=True \
+    --learning-rate=0.1 \
+    --mini-batch-size=128 \
+    --print-freq=1024 \
+    --print-time \
+    --test-mini-batch-size=16384 \
+    --test-num-workers=16 \
+    2>&1 | tee 224-112-1_lr0.1.log
+
+# CR=28.7%
+python dlrm_s_pytorch.py \
+    --arch-sparse-feature-size=16 \
+    --arch-mlp-bot="13-512-256-64-16" \
+    --arch-mlp-top="192-96-1" \
+    --data-generation=dataset \
+    --data-set=kaggle \
+    --raw-data-file=/hdd3/jiayi/kaggle/train.txt \
+    --processed-data-file=/hdd3/jiayi/kaggle/kaggleAdDisplayChallenge_processed.npz \
+    --loss-function=bce \
+    --round-targets=True \
+    --learning-rate=0.1 \
+    --mini-batch-size=128 \
+    --print-freq=1024 \
+    --print-time \
+    --test-mini-batch-size=16384 \
+    --test-num-workers=16 \
+    2>&1 | tee 192-96-1_lr0.1.log
+
+# CR=18%
+python dlrm_s_pytorch.py \
+    --arch-sparse-feature-size=16 \
+    --arch-mlp-bot="13-512-256-64-16" \
+    --arch-mlp-top="128-64-1" \
+    --data-generation=dataset \
+    --data-set=kaggle \
+    --raw-data-file=/hdd3/jiayi/kaggle/train.txt \
+    --processed-data-file=/hdd3/jiayi/kaggle/kaggleAdDisplayChallenge_processed.npz \
+    --loss-function=bce \
+    --round-targets=True \
+    --learning-rate=0.1 \
+    --mini-batch-size=128 \
+    --print-freq=1024 \
+    --print-time \
+    --test-mini-batch-size=16384 \
+    --test-num-workers=16 \
+    2>&1 | tee 128-64-1_lr0.1.log
