472ee37eb710e79d46cf906115f7280099bfd11f
htwang14 <htwang14@gmail.com>
2020-09-16 18:08:58 -0500

    tracking all log output files

--- baseline.sh
+++ baseline.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# CR=49.6%
+# 432 -> 512 -> 256- > 1
+# FLOPs = 2*432*512+512+2*512*256+256+2*256+1 = 705793
+
+# FLOPs = 2*432*300+300+2*300*150+150+2*150+1 = 349951
+# CR=0.4958 best 78.797 %
 python dlrm_s_pytorch.py \
     --arch-sparse-feature-size=16 \
     --arch-mlp-bot="13-512-256-64-16" \
@@ -40,7 +44,8 @@ python dlrm_s_pytorch.py \
     --arch-interaction-op cat \
     2>&1 | tee 256-128-1_lr0.1.log
 
-# CR=%
+# FLOPs = 2*432*240+240+2*240*120+2*120+120+1 = 265561
+# CR=0.3763 best 78.797 %
 python dlrm_s_pytorch.py \
     --arch-sparse-feature-size=16 \
     --arch-mlp-bot="13-512-256-64-16" \
@@ -60,7 +65,8 @@ python dlrm_s_pytorch.py \
     --arch-interaction-op cat \
     2>&1 | tee 240-120-1_lr0.1.log
 
-# CR=34.6%
+# FLOPs = 2*432*224+224+2*224*112+112+2*112+1 = 122305
+# CR=0.3439 best 78.777 %
 python dlrm_s_pytorch.py \
     --arch-sparse-feature-size=16 \
     --arch-mlp-bot="13-512-256-64-16" \
@@ -80,7 +86,8 @@ python dlrm_s_pytorch.py \
     --arch-interaction-op cat \
     2>&1 | tee 224-112-1_lr0.1.log
 
-# CR=28.7%
+# FLOPs = 2*432*192+192+2*192*96+96+2*96+1 = 203233
+# CR=28.79%
 python dlrm_s_pytorch.py \
     --arch-sparse-feature-size=16 \
     --arch-mlp-bot="13-512-256-64-16" \
@@ -100,7 +107,8 @@ python dlrm_s_pytorch.py \
     --arch-interaction-op cat \
     2>&1 | tee 192-96-1_lr0.1.log
 
-# CR=18%
+# FLOPs = 2*432*128+128+2*128*64+64+2*64+1 = 127297
+# CR=18.04%
 python dlrm_s_pytorch.py \
     --arch-sparse-feature-size=16 \
     --arch-mlp-bot="13-512-256-64-16" \
@@ -119,3 +127,46 @@ python dlrm_s_pytorch.py \
     --test-num-workers=16 \
     --arch-interaction-op cat \
     2>&1 | tee 128-64-1_lr0.1.log
+
+# FLOPs = 2*432*64+64+2*64*32+32+2*32+1 = 59553
+# CR=8.44%
+python dlrm_s_pytorch.py \
+    --arch-sparse-feature-size=16 \
+    --arch-mlp-bot="13-512-256-64-16" \
+    --arch-mlp-top="64-32-1" \
+    --data-generation=dataset \
+    --data-set=kaggle \
+    --raw-data-file=/hdd3/jiayi/kaggle/train.txt \
+    --processed-data-file=/hdd3/jiayi/kaggle/kaggleAdDisplayChallenge_processed.npz \
+    --loss-function=bce \
+    --round-targets=True \
+    --learning-rate=0.1 \
+    --mini-batch-size=128 \
+    --print-freq=1024 \
+    --print-time \
+    --test-mini-batch-size=16384 \
+    --test-num-workers=16 \
+    --arch-interaction-op cat \
+    2>&1 | tee 64-32-1_lr0.1.log
+
+
+# FLOPs = 2*432*32+32+2*32*16+16+2*16+1 = 28753
+# CR=4.07%
+python dlrm_s_pytorch.py \
+    --arch-sparse-feature-size=16 \
+    --arch-mlp-bot="13-512-256-64-16" \
+    --arch-mlp-top="32-16-1" \
+    --data-generation=dataset \
+    --data-set=kaggle \
+    --raw-data-file=/hdd3/jiayi/kaggle/train.txt \
+    --processed-data-file=/hdd3/jiayi/kaggle/kaggleAdDisplayChallenge_processed.npz \
+    --loss-function=bce \
+    --round-targets=True \
+    --learning-rate=0.1 \
+    --mini-batch-size=128 \
+    --print-freq=1024 \
+    --print-time \
+    --test-mini-batch-size=16384 \
+    --test-num-workers=16 \
+    --arch-interaction-op cat \
+    2>&1 | tee 32-16-1_lr0.1.log
