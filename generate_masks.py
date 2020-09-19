a6e4cec8f9c140bc9fa6c8a7ed3bc995beb0aad5
htwang14 <htwang14@gmail.com>
2020-09-19 16:50:34 -0500

    save masks as list instead of dict in pkl

--- generate_masks.py
+++ generate_masks.py
@@ -24,26 +24,41 @@ score_sorted = np.sort(score)[::-1]
 score_th = score_sorted[int(args.ratio * len(score))]
 # print('score_sorted:', score_sorted)
 print('score_th:', score_th)
-mask = (score > score_th)
-print('mask:', mask.shape, mask[0:10])
+prune_bin = (score <= score_th) # 1: prune; 0: keep
+print('prune_bin:', prune_bin.shape, prune_bin[0:10])
 
-masks = np.array_split(mask, split_ids)
-mask_dict = {}
-for mask, (name, _) in zip(masks, importance_score.items()):
-    print(name, mask.shape)
-    mask_dict[name] = mask
-    print('mask:', mask.shape)
+prune_bins = np.array_split(prune_bin, split_ids)
+
+prune_bins_w3 = prune_bins[0]
+prune_bins_w2 = prune_bins[1]
+prune_bins_w1 = prune_bins[2]
+print('prune_bins_w3:', prune_bins_w3.shape)
+print('prune_bins_w2:', prune_bins_w2.shape)
+print('prune_bins_w1:', prune_bins_w1.shape)
+m1 = np.ones((512, 432))
+m1[prune_bins_w1,:] = 0
+m2 = np.ones((256, 512))
+m2[prune_bins_w2,:] = 0
+m2[:,prune_bins_w1] = 0
+m3 = np.ones((1, 256))
+m3[:,prune_bins_w2] = 0
+mask_list = [m1, m2, m3]
 
 # find flops
-c1 = np.sum(masks[2])
-c2 = np.sum(masks[1])
+c1 = 512-np.sum(prune_bins_w1)
+c2 = 256-np.sum(prune_bins_w2)
 FLOPs = int(2*432*c1 + c1 + 2*c1*c2 + c2 + 2*c2*1 + 1)
-print(c1, c2, FLOPs/705793)
+print(c1, c2, FLOPs, FLOPs/705793)
  
 # save mask
 save_dir = os.path.join('masks', args.metric)
 if not os.path.isdir(save_dir):
     os.makedirs(save_dir)
 f = open(os.path.join(save_dir, '%d_%s.pkl' % (FLOPs, args.ratio)), "wb")
-pickle.dump(mask_dict, f)
+pickle.dump(mask_list, f)
 f.close()
+
+f = open(os.path.join(save_dir, '%d_%s.pkl' % (FLOPs, args.ratio)), "rb")
+mask_list = pickle.load(f)
+for mask in mask_list:
+    print(mask.shape)
